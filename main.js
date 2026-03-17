f5e015e5e26c7015ea72b5d5a789bc5e1297c148
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-17 14:06:11 +0100

    exercice drumpad 1ere partie corrige

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
+let padKeys = document.querySelectorAll(".key");
+// console.log(padKeys);
+
 addEventListener("keydown", handleAudioListener);
 addEventListener("keyup", handleStopEffect);
 
 function handleAudioListener(event) {
   console.log(event);
-  
+
   // récupère la valeur de l'attribut data-key du bouton cliqué
   let keyValue = event.keyCode;
   // cherche dans le html un élément <audio> qui possède le même data-key que le bouton
   let audio = document.querySelector(`audio[data-key="${keyValue}"]`);
+
+  if (event.repeat) return;
+  if (!audio) return;
+
+  audio.currentTime = 0;
   audio.play();
 
   let padKey = document.querySelector(`div[data-key="${keyValue}"]`);
   console.log(padKey);
   padKey.classList.add("playing");
- 
 }
-aa
+// }
+
 function handleStopEffect(event) {
-  console.log(event);
-  
+  // console.log(event);
+
   let keyValue = event.keyCode;
-  let audio = document.querySelector(`audio[data-key="${keyValue}"]`);
-  audio.play();
+
   let padKey = document.querySelector(`div[data-key="${keyValue}"]`);
   console.log(padKey);
-   padKey.classList.remove("playing");
+  if (!padKey) return;
+  padKey.classList.remove("playing");
 }
